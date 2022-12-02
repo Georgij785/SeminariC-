e22bf40b0dca22f3a729f73df15ceb8015a4cdd6
george <georgij5ptb@gmail.com>
2022-12-02 17:42:22 +0300

    Сделали первое задание (task1)

--- seminar2/task1/Program.cs
+++ seminar2/task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int Promt(string message)
+{
+    System.Console.Write($"{message} >");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+int number = (Promt("Напишите ваше ТРЁХЗНАЧНОЕ число"));
+int result = number / 10 % 10;
+System.Console.WriteLine($"2-я цифра вашего(надеюсь трёхзначного) числа {result}");
