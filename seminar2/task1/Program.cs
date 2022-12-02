771c71b1619417e7b7f9321f0e9d4926fa090243
george <georgij5ptb@gmail.com>
2022-12-02 18:27:16 +0300

    Подправили и добавили функцию

--- seminar2/task1/Program.cs
+++ seminar2/task1/Program.cs
@@ -11,6 +11,23 @@ int Promt(string message)
     int answer = Convert.ToInt32(Console.ReadLine());
     return answer;
 }
-int number = (Promt("Напишите ваше ТРЁХЗНАЧНОЕ число"));
-int result = number / 10 % 10;
-System.Console.WriteLine($"2-я цифра вашего(надеюсь трёхзначного) числа {result}");
+int number = (Promt("Напишите ваше число"));
+
+bool ValidateNumber(int number)
+{
+    if (99 < number && number < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Число не трёхзначное");
+    return false;
+}
+
+if (ValidateNumber(number))
+{
+    int result = number / 10 % 10;
+    System.Console.WriteLine($"2-я цифра вашего числа {result}");
+}
+
+
+
