4eff9ea2452e4fe8f5d2221584fca2884b602aa6
george <georgij5ptb@gmail.com>
2022-12-02 18:47:36 +0300

    выполнили 2-е задание

--- seminar2/task2/Program.cs
+++ seminar2/task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int DELETE2NUMBER(int number)
+{
+    int desatki = number / 10 % 10;
+    number = number - desatki * 10;
+    int edinisi = number % 10;
+    number=number/10;
+    number= number+edinisi;
+    return number;
+}
+int number=new Random().Next(100, 999);
+System.Console.WriteLine("Изначальноу сгенерированное число > " + number);
+System.Console.WriteLine("оно-же, но без 2-й цифры > "+DELETE2NUMBER(number));
