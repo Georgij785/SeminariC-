3dec76fccfbf196a2e49c573b3132c30f4012ae8
george <georgij5ptb@gmail.com>
2022-12-03 07:45:15 +0300

    выполнили 3-е задание

--- seminar2/task3/Program.cs
+++ seminar2/task3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите ваше число >");
+int number = Convert.ToInt32(Console.ReadLine());
+int TOTHREE(int number)
+{
+    while (number > 999)
+    {
+        number = number / 10; 
+    }
+    int result=number%10;
+    return result;
+}
+if (number<100)
+{
+    System.Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    System.Console.WriteLine("Третья цифра с начала > "+TOTHREE(number));
+}
+
