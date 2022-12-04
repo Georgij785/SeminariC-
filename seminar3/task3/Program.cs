222fb366f9c2a9a5d611b6633c4cf9d5850dac51
george <georgij5ptb@gmail.com>
2022-12-04 10:04:48 +0300

    поменяли квадрат на куб и положили действия в метод

--- seminar3/task3/Program.cs
+++ seminar3/task3/Program.cs
@@ -1,14 +1,20 @@
 ﻿// Задача 3
-
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+void Kub(int Number)
+{
+    int count = 1;
+    while (count <= Number)
+    {
+        int result = count * count * count;
+        count++;
+        System.Console.WriteLine(result);
+    }
+}
+
 System.Console.Write("Введите ваше число  >");
 int N = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-while (count <= N)
-{
-    System.Console.WriteLine(count * count);
-    count++;
-}
+
+Kub(N);
