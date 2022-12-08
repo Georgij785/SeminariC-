7951d6d355484b26785168bfc8f44dc0a850ee2c
george <georgij5ptb@gmail.com>
2022-12-08 18:26:53 +0300

    Сделали второе задание

---
+++ seminar5/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int SumChetint(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        result+=array[i];
+    }
+    return result;
+}
+int[] FillList(int[] list, int Down, int Up)
+{
+    for (int i = 0; i < list.Length; i++)
+    {
+        list[i] = new Random().Next(Down, Up);
+    }
+    return list;
+}
+
+int[] array=new int[10];
+System.Console.WriteLine(SumChetint(FillList(array,-100,101)));
