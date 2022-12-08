f631aff4ad5c9c462ea0742681f215281c3f71bc
george <georgij5ptb@gmail.com>
2022-12-08 19:09:33 +0300

    Сделали 2-е задание

---
+++ seminar5/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿double MaxMin(double[] list)
+{
+    double max = list[0];
+    double min = list[0];
+    for (int i = 0; i < list.Length; i++)
+    {
+        if (list[i] > max) max = list[i];
+        else
+        {
+            if (list[i] < min)
+            {
+                min = list[i];
+            }
+        }
+    }
+    double result = max - min;
+    return result;
+}
+
+double[] array = { 7.6, 34, 4.456465656, 7.0, 23.5 };
+System.Console.WriteLine(MaxMin(array));
