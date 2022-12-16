2ccb718629d09ac8d0c7a355b515cabf1221c835
george <georgij5ptb@gmail.com>
2022-12-16 18:17:12 +0300

    сделали первое задание

---
+++ seminar6/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintList(int[] list)
+{
+    for (int i = 0; i < list.Length; i++)
+    {
+        System.Console.WriteLine(list[i]);
+    }
+}
+int[] Rotate(int[] list)
+{
+    int count=1;
+    int number=0;
+    for (int i = 0; i <list.Length/2; i++)
+    {
+        number=list[i];
+        list[i]=list[list.Length-count];
+        list[list.Length-count]=number;
+        count++;
+    }
+    return list;
+}
+
+int[] array={1,2,3,4,5};
+PrintList(array);
+System.Console.WriteLine();
+
+PrintList(Rotate(array));
