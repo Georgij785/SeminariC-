24136d2747a00de3e57c2b4799df28d168d59593
george <georgij5ptb@gmail.com>
2022-12-16 19:02:44 +0300

    сделали второе задание

---
+++ seminar6/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет,
+//  может ли существовать 
+// треугольник с сторонами такой длины.
+// Функция вывода приглашения и ввода целого числа
+int Prompt(string message)
+{
+    Console.Write($"{message} >");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
+bool CheckSizes(int A,int B,int C)
+{
+    if(A+B>C)
+    {
+        return true;
+    }
+    return false;
+}
+
+int A =Prompt("Введите длину первой стороны >");
+int B =Prompt("Введите длину второй стороны >");
+int C =Prompt("Введите длину третьей стороны >");
+if (CheckSizes(A,B,C)&&CheckSizes(B,C,A)&&CheckSizes(A,C,B))
+{
+    System.Console.WriteLine("Да, такой треугольник может существовать.");
+}
+else
+{
+    System.Console.WriteLine("Нет, такого треугольника нет.");
+}
