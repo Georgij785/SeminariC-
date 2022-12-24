2d2cd232bc4c3121297025870384e1bed8051478
george <georgij5ptb@gmail.com>
2022-12-24 22:44:39 +0300

    сделали первое задание

---
+++ seminar9/HomeTask/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет 
+// все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+bool Validate(int M, int N)
+{
+    if (N > M)
+    {
+        return true;
+    }
+    return false;
+}
+void FromMToNDelitel2Plus(int M, int N)
+{
+    if (M > 0)
+    {
+        if (M == N || (M + 1) == N)
+        {
+            if ((M % 2 )==1)
+            {
+                M+=1;
+            }
+            System.Console.WriteLine(M);
+            return;
+
+        }
+        System.Console.WriteLine(M);
+
+    }
+    if ((M % 2) == 1||(M % 2) == -1)
+    {
+        M+=1;
+    }
+    
+    FromMToNDelitel2Plus(M+2, N);
+}
+
+
+
+int M = 30;
+int N = 10;
+if (Validate(M, N))
+{
+    FromMToNDelitel2Plus(M, N);
+}
+else
+{
+    FromMToNDelitel2Plus( N,M);
+}
